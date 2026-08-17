bdedae4ba971f1e1d3af17d429f61f395b8935f5
mariana-mbs <mariana-mbs@outlook.com>
2026-08-17 20:36:14 -0300

    Resolve o exercício 03 - área e perímetro do retângulo

---
+++ Exercicio03/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Digite a base: ");
+double baseRetangulo = double.Parse(Console.ReadLine());
+
+Console.Write("Digite a altura: ");
+double alturaRetangulo = double.Parse(Console.ReadLine());
+
+double area = baseRetangulo * alturaRetangulo;
+double perimetro = 2 * (baseRetangulo + alturaRetangulo);
+
+Console.WriteLine($"Área do retângulo: {area}");
+Console.WriteLine($"Perímetro do retângulo: {perimetro}");
+
