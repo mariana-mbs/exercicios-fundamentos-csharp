c05b142c34de537f4b9496d4db7049ea8e1cbd37
mariana-mbs <mariana-mbs@outlook.com>
2026-08-17 20:46:53 -0300

    Resolve o exercício 04 - conversor de moedas

---
+++ Exercicio04/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Digite um valor em real: R$ ");
+double valorReal = double.Parse(Console.ReadLine());
+
+Console.Write("Digite a cotação do dólar: R$ ");
+double cotacaoDolar = double.Parse(Console.ReadLine());
+
+double valorDolar = valorReal / cotacaoDolar;
+Console.WriteLine($"Valor em dólares: U$ {valorDolar:F2}");
