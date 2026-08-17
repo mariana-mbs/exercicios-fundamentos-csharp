6d471113e3b3cb880f1b1283258bb26e58b196ad
mariana-mbs <mariana-mbs@outlook.com>
2026-08-17 20:55:11 -0300

    Resolve o exercício 05 - calculadora simples

---
+++ Exercicio05/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Digite um número: ");
+double numero1 = double.Parse(Console.ReadLine());
+
+Console.Write("Digite outro número: ");
+double numero2 = double.Parse(Console.ReadLine());
+
+double soma = numero1 + numero2;
+double subtracao = numero1 - numero2;
+double multiplicacao = numero1 * numero2;
+double divisao = numero1 / numero2;
+
+Console.WriteLine($"Soma: {soma}");
+Console.WriteLine($"Subtração: {subtracao}");
+Console.WriteLine($"Multiplicação: {multiplicacao}");
+Console.WriteLine($"Divisão: {divisao}");
