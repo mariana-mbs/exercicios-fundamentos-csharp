dd5875dfaa37fb564bc211430bcb5d07115b31a3
mariana-mbs <mariana-mbs@outlook.com>
2026-08-17 19:59:54 -0300

    Resolve o exercício 02 - antecessor e sucessor

---
+++ Exercicio02/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Digite um número: ");
+int numero = int.Parse(Console.ReadLine());
+
+int antecessor = numero - 1;
+int sucessor = numero + 1;
+
+Console.WriteLine($"O antecessor de {numero} é {antecessor}, e o sucessor é {sucessor}");
