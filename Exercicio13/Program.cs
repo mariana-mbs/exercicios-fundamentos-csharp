090b49a2100e215ee1d54c0b8245d3a570696811
mariana-mbs <mariana-mbs@outlook.com>
2026-08-17 22:50:47 -0300

    Resolve o exercício 13 - verificador de senha

---
+++ Exercicio13/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Digite uma senha: ");
+string senha = Console.ReadLine();
+
+while (senha.Length < 8)
+{
+    Console.WriteLine("Senha inválida! A senha deve ter pelo menos 8 caracteres.");
+    Console.WriteLine("Digite uma senha: ");
+    senha = Console.ReadLine();
+}
+Console.WriteLine("Senha válida!");
