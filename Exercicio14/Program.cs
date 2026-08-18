f6ec9788389ca8c2df00eac0b642da57d1c14ee9
mariana-mbs <mariana-mbs@outlook.com>
2026-08-17 23:05:19 -0300

    Resolve o exercício 14 - maior elemento

---
+++ Exercicio14/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Digite o 1º número: ");
+int maior = int.Parse(Console.ReadLine());
+int posicaoMaior = 1;
+
+for (int i = 2; i <= 10; i++)
+{
+    Console.Write($"Digite o {i}º número: ");
+    int numero = int.Parse(Console.ReadLine());
+
+    if (numero > maior)
+    {
+        maior = numero;
+        posicaoMaior = i;
+    }
+}
+Console.WriteLine($"O maior número é {maior} e está na posição {posicaoMaior}.");
