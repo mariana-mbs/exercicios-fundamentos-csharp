b04c006c9fb2b1d5a0355bb31b6ede29c51f5e04
mariana-mbs <mariana-mbs@outlook.com>
2026-08-17 21:46:42 -0300

    Corrige o exercício 07 e adiciona o exercício 08

--- Exercicio07/Program.cs
+++ Exercicio07/Program.cs
@@ -1,7 +1,4 @@
-﻿Console.Write("Digite um número: ");
-int numero = int.Parse(Console.ReadLine());
-
-for (int i = 1; i <= 10; i++)
+﻿for (int i = 50; i >= 1; i--)
 {
-    Console.WriteLine($"{numero} x {i} = {numero * i}");
+    Console.WriteLine(i);
 }
