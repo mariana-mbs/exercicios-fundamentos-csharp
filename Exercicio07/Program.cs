5ca000c79fd2228ebc8cc197233ddb927780697a
mariana-mbs <mariana-mbs@outlook.com>
2026-08-17 21:38:30 -0300

    Resolve o exercício 07 - tabuada

--- Exercicio07/Program.cs
+++ Exercicio07/Program.cs
@@ -1,4 +1,7 @@
-﻿for (int i = 50; i >= 1; i--)
+﻿Console.Write("Digite um número: ");
+int numero = int.Parse(Console.ReadLine());
+
+for (int i = 1; i <= 10; i++)
 {
-    Console.WriteLine(i);
+    Console.WriteLine($"{numero} x {i} = {numero * i}");
 }
