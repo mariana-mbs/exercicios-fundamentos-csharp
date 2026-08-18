bc8bb2f965fda5e6e8b7b717d28d2be771bbbd1c
mariana-mbs <mariana-mbs@outlook.com>
2026-08-17 23:17:37 -0300

    Resolve o exercício 15 - média de um vetor

---
+++ Exercicio15/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Digite uma nota: ");
+double nota = double.Parse(Console.ReadLine());
+double somaNotas = nota;
+
+for (int i = 1; i < 8; i++)
+{
+    Console.Write($"Digite a {i + 1}ª nota: ");
+    nota = double.Parse(Console.ReadLine());
+    
+    somaNotas += nota;
+}
+
+double mediaNotas = somaNotas / 8;
+Console.WriteLine("A média das notas é: " + mediaNotas);
