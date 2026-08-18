a7351ab22bb0668a059b70d5a4ae63c5439bd19b
mariana-mbs <mariana-mbs@outlook.com>
2026-08-17 23:43:34 -0300

    Resolve o exercício 18 - jogo da senha

---
+++ Exercicio18/Program.cs
@@ -0,0 +1,22 @@
+﻿Random random = new Random();
+int numeroSorteado = random.Next(1, 101);
+
+Console.Write("Digite um número entre 1 e 100: ");
+int numero = int.Parse(Console.ReadLine());
+
+while (numero != numeroSorteado)
+{
+    if (numero > numeroSorteado)
+    {
+        Console.WriteLine("O número digitado é maior que o número sorteado.");
+    }
+    else
+    {
+        Console.WriteLine("O número digitado é menor que o número sorteado.");
+    }
+
+    Console.Write("Tente novamente: ");
+    numero = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("Você acertou!");
