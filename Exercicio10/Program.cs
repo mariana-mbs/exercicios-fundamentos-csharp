bd88109ad69e0413b64be743ec7faceef24c834a
mariana-mbs <mariana-mbs@outlook.com>
2026-08-17 22:25:28 -0300

    Resolve o exercício 10 - login simples

---
+++ Exercicio10/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Digite o usuário: ");
+string usuario = Console.ReadLine();
+
+Console.Write("Digite sua senha: ");
+string senha = Console.ReadLine();
+
+while (senha != "abcde")
+{
+    Console.Write("Senha incorreta, digite novamente: ");
+    senha = Console.ReadLine();
+}
+
+Console.WriteLine($"Bem-vindo, {usuario}!");
