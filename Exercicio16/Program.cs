451be420244288f7533e3c2a4c5e906a0a54f87c
mariana-mbs <mariana-mbs@outlook.com>
2026-08-17 23:32:06 -0300

    Resolve o exercício 16 - contagem de pares

---
+++ Exercicio16/Program.cs
@@ -0,0 +1,14 @@
+﻿int saoPares = 0;
+
+for (int i = 1; i <= 20; i++)
+{
+    Console.Write($"Digite o {i}º número: ");
+    int numero = int.Parse(Console.ReadLine());
+
+    if (numero % 2 == 0)
+    {
+        saoPares++;
+    }
+}
+
+Console.WriteLine($"Quantidade de números pares: {saoPares}");
