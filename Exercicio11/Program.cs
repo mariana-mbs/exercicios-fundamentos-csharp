cccf6dd82ac62ade6663d638c37edf257480dc1a
mariana-mbs <mariana-mbs@outlook.com>
2026-08-17 22:38:48 -0300

    Resolve o exercício 11 - somar até digitar zero

---
+++ Exercicio11/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Digite um número inteiro: ");
+int numero = int.Parse(Console.ReadLine());
+int soma = 0;
+
+while (numero != 0)
+{
+    Console.Write("Digite outro número, ou 0 para sair e somar: ");
+    soma += numero;
+    numero = int.Parse(Console.ReadLine());
+    
+}
+Console.WriteLine($"A soma dos números digitados é: {soma}");
