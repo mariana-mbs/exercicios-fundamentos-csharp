9cc5684ef0489e04b854654eae6373b4b0b9d36f
mariana-mbs <mariana-mbs@outlook.com>
2026-08-17 23:35:46 -0300

    Resolve o exercício 17 - ordem inversa

---
+++ Exercicio17/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] numeros = new int[10];
+
+for (int i = 0; i < 10; i++)
+{
+    Console.Write($"Digite o {i + 1}º número: ");
+    numeros[i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("Números na ordem inversa:");
+
+for (int i = 9; i >= 0; i--)
+{
+    Console.WriteLine(numeros[i]);
+}
